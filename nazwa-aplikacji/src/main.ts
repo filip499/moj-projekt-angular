b493a3ad64bb24e8ef61a46ed91d68c40c3c5734
Filip Mikiciuk <filip.mikiciukk@gmail.com>
2026-04-28 13:34:38 +0200

    naprawa tsconfig

--- nazwa-aplikacji/src/main.ts
+++ nazwa-aplikacji/src/main.ts
@@ -1,6 +1,7 @@
+import 'zone.js'; // Dodaj to na samym początku pliku!
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app'; // Upewnij się, że tu jest 'App', a nie 'AppComponent'
+import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
